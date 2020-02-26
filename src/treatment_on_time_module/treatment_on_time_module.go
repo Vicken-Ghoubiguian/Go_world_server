4d9d4f1cb4f813f908ec5e3dcf8d4ff0e87ae384
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-02-26 08:26:26 +0100

    Addition of intg shaping function

--- src/treatment_on_time_module/treatment_on_time_module.go
+++ src/treatment_on_time_module/treatment_on_time_module.go
@@ -1,13 +1,25 @@
 package treatment_on_time_module
 
 import (
-        //"strings"
         "time"
 	"strconv"
 )
 
+func int_shaping_function(int_to_treat int) string {
+
+	if int_to_treat < 10 {
+
+		return "0" + strconv.Itoa(int_to_treat)
+
+	} else {
+
+		return strconv.Itoa(int_to_treat)
+
+	}
+}
+
 func Time_shaping_function(current_time_for_tz time.Time) string {
 
-	return strconv.Itoa(current_time_for_tz.Hour()) + ":" + strconv.Itoa(current_time_for_tz.Minute()) + ":" + strconv.Itoa(current_time_for_tz.Second()) + " " + strconv.Itoa(current_time_for_tz.Day()) + "/" + strconv.Itoa(int(current_time_for_tz.Month())) + "/" + strconv.Itoa(current_time_for_tz.Year())
+	return int_shaping_function(current_time_for_tz.Hour()) + ":" + int_shaping_function(current_time_for_tz.Minute()) + ":" + int_shaping_function(current_time_for_tz.Second()) + " " + int_shaping_function(current_time_for_tz.Day()) + "/" + int_shaping_function(int(current_time_for_tz.Month())) + "/" + int_shaping_function(current_time_for_tz.Year())
 
 }
