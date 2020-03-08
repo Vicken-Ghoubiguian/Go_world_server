429bac3681a17bf4e7c76703446a9829da3e1f93
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-03-08 17:31:47 +0100

    Deployment of logs in konsole

--- src/template_displayer_module/template_displayer_module.go
+++ src/template_displayer_module/template_displayer_module.go
@@ -3,6 +3,8 @@ package template_displayer_module
 import (
 	"html/template"
 	"net/http"
+	"logs_module"
+	"colors_in_terminal_module"
 	"log"
 )
 
@@ -25,5 +27,12 @@ func Display_template_function(defined_page Page, w http.ResponseWriter) {
 	if err != nil {
 
 		log.Fatalf("Template execution: %s", err)
+
+		logs_module.Writing_log_in_terminal_function(colors_in_terminal_module.Red, "An error occured, consult logs file to know more...")
+
+	} else {
+
+		logs_module.Writing_log_in_terminal_function(colors_in_terminal_module.Green, "Display timezones...")
+
 	}
 }
