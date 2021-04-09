130198537242459fd1953397b7cf0c09d6b76d93
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-04-09 18:40:48 +0200

    Tooltips

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
 $( function() {
 	
 	$( "#tz_displayer_as_accordion" ).accordion();
+
+	$('[data-toggle="tooltip"]').tooltip({
+
+			show: {
+
+        		effect: "slideDown",
+        		delay: 250
+      		},
+
+      		hide: {
+
+        		effect: "slideUp",
+        		delay: 250
+      		}
+  	});
 });
